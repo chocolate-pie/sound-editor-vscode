fe925613868fc859b9e63d9625abeeabb02cb732
chocolate-pie <106949016+Chocolate-pie@users.noreply.github.com>
2023-02-08 10:57:06 +0900

    Add Logo And Setup Gulp :hammer:

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+const raster = require('gulp-raster');
+const rename = require('gulp-rename');
+const gulp = require('gulp');
+gulp.task('default', () => {
+     return gulp.src('./logo/*.svg')
+    .pipe(raster())
+    .pipe(rename({extname: '.png'}))
+    .pipe(gulp.dest('./dist'));
+});
